4bb33e8eeae1df853451d3a75274330be0279efc
julian <juliancamargo00@gmail.com>
2023-04-13 12:02:02 -0500

    proyectokpt

---
+++ empleado/js/empleado.js
@@ -0,0 +1,43 @@
+const platos = document.querySelector(".platos");
+platos.minlenght = 4;
+platos.required = "required";
+
+const cantidad = document.querySelector(".cantidad");
+cantidad.type = "number";
+cantidad.required = "required"
+cantidad.minlenght = 4;
+
+cantidad.required = "required";
+
+const comentarios = document.querySelector(".comentarios");
+
+
+
+
+
+
+
+const empleado = document.querySelector(".empleado");
+empleado.required = "required";
+
+const cliente = document.querySelector(".cliente");
+cliente.required = "required"
+cliente.minlenght = 4;
+
+
+const valor = document.querySelector(".valor");
+valor.required = "required";
+
+
+const fecha = document.querySelector(".fecha");
+fecha.required = "required";
+
+
+const hora = document.querySelector(".hora");
+hora.required = "required";
+
+
+
+
+
+
---
+++ js/reserva1.js
@@ -0,0 +1,16 @@
+const nom = document.querySelector(".nombre");
+
+
+nom.required = "required";
+
+const numero = document.querySelector(".doc");
+numero.type = "number";
+numero.minlenght = 4;
+
+numero.required = "required";
+
+const email = document.querySelector(".correo");
+email.required = "required";
+
+const telefono = document.querySelector(".tel");
+telefono.required = "required";
