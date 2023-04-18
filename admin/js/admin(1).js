106949feedd4e0d7843eee2bd85a130427ed4ddd
unknown <SENA@BOGAPRCSFFSP399.FormacionSena.RED>
2023-04-18 11:46:46 -0500

    Actualizacion del proyecto

---
+++ admin/js/admin(1).js
@@ -0,0 +1,26 @@
+const doc = document.querySelector(".doc");
+doc.type = "number";
+doc.minlenght = 4;
+doc.required = "required";
+
+const nombre = document.querySelector(".nombre");
+nombre.required = "required"
+nombre.minlenght = 4;
+
+
+const telefono = document.querySelector(".telefono");
+telefono.required = "required"
+
+
+const horario = document.querySelector(".horario");
+horario.required = "required"
+
+
+const salario = document.querySelector(".salario");
+salario.required = "required"
+
+
+const experiencia = document.querySelector(".experiencia");
+
+
+
